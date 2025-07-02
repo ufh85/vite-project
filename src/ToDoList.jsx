e7ca9d6f3cd796e29fd3a9dc700080eea8fc6f9c
ufh85 <uberskull85@gamail.com>
2025-07-02 18:00:25 +0000

    Edited the look of the task list components

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ToDoList({toDos, handleAdd, handleRemove}) {
+    const [ text, setText ] = useState('');
+
+    const handleChange = ({target}) => {
+        setText(target.value);
+    }
     
     const handleSubmit = (event) => {
         event.preventDefault();
         handleAdd(event.target.taskName.value);
+        setText('');
     }
 
     const handleRemoveClick = (id) => {
@@ -12,13 +18,20 @@ function ToDoList({toDos, handleAdd, handleRemove}) {
     }
     
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input type='text' name='taskName' />
-                <input type='submit' value='Add' />
-            </form>
+        <>  
+            <div className='formContainer'>
+                <form className='toDoForm' onSubmit={handleSubmit}>
+                    <div>
+                        <h4 className='toDoFormLabel'>Task list</h4>
+                    </div>
+                    <div className='taskFormInput'>
+                        <input type='text' name='taskName' onChange={handleChange} value={text} />
+                        <input type='submit' value='Add' />
+                    </div>
+                </form>
+            </div>
             <div>
-                {toDos.map((item, i)=> <div key={'to_do_' + i}>{item.text}<input type='button' onClick={() => handleRemoveClick(item.id)} value='-' /></div>)}
+                {toDos.map((item, i)=> <div className='toDoItem' key={'to_do_' + i}>{item.text}<input type='button' onClick={() => handleRemoveClick(item.id)} value='-' /></div>)}
             </div>
         </>
     )
